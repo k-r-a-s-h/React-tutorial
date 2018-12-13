8ba4733b820908d03193e4408bfa14229f5534dc
k_r_a_s_h_ <krishna05564@gmail.com>
2018-12-13 14:28:55 +0530

    l38

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar'
-import {BrowserRouter,Route} from 'react-router-dom'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import Home from './components/home';
 import About from './components/about';
-import Contact from './components/contact' 
+import Contact from './components/contact'
+import Post from './components/post'; 
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- poketimes/src/components/home.js
+++ poketimes/src/components/home.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png';
 
 class Home extends Component{
     state ={
@@ -18,9 +20,13 @@ class Home extends Component{
         const postList = posts.length ? (
             posts.map(post => {
                 return (
-                    <div className="post card" key={post.id}>
+                    <div className="post card hoverable" key={post.id}>
+                        <img src={Pokeball} alt="a pokeball"/>
                         <div className="card-content">
-                            <span className="card-title">{post.title}</span>
+                            <Link to={'/'+post.id}>
+                            <span className="card-title red-text">{post.title}</span>
+                            </Link>
+                            
                             <p>{post.body}</p>
                         </div>
                     </div>
@@ -31,7 +37,7 @@ class Home extends Component{
         )
         return(
 
-            <div className="container">
+            <div className="container home ">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
